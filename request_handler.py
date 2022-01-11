0e8c9c06275e95395402cb80272f4204f54c8a0c
jmason87 <joewebdev87@gmail.com>
2022-01-11 09:50:10 -0600

    through ch. 4

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,8 @@
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from views import get_all_animals, get_single_animal, get_all_locations, get_single_location
 from views import get_all_employees, get_single_employee, get_all_customers, get_single_customer
+from views import create_animal, create_location, create_employee, create_customer
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -94,19 +96,38 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
     def do_POST(self):
-        """Handles POST requests to the server
-        """
-        # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
 
-    # Here's a method on the class that overrides the parent's method.
-    # It handles any PUT request.
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
 
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_animal = None
+        new_location = None
+        new_employee = None
+        new_customer = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+        if resource == "locations":
+            new_animal = create_location(post_body)
+        if resource == "employees":
+            new_animal = create_employee(post_body)
+        if resource == "customers":
+            new_animal = create_customer(post_body)
+        # Encode the new animal and send in response
+        self.wfile.write(f"{new_animal}".encode())
+        self.wfile.write(f"{new_location}".encode())
+        self.wfile.write(f"{new_employee}".encode())
+        self.wfile.write(f"{new_customer}".encode())
     def do_PUT(self):
         """Handles PUT requests to the server
         """
