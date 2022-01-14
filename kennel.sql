aa62219067657bb86dd89e338b7796fd55439a67
jmason87 <joewebdev87@gmail.com>
2022-01-14 12:05:47 -0600

    through chapter 9, first queries SELECT FROM WHERE

--- kennel.sql
+++ kennel.sql
@@ -55,3 +55,39 @@ INSERT INTO `Animal` VALUES (null, "Jax", "Treatment", "Beagle", 1, 1);
 INSERT INTO `Animal` VALUES (null, "Falafel", "Treatment", "Siamese", 4, 2);
 INSERT INTO `Animal` VALUES (null, "Doodles", "Kennel", "Poodle", 3, 1);
 INSERT INTO `Animal` VALUES (null, "Daps", "Kennel", "Boxer", 2, 2);
+
+-- Get only the animal rows where the `id` field value is 3
+SELECT
+    a.id,
+    a.name,
+    a.breed,
+    a.status,
+    a.location_id,
+    a.customer_id
+FROM animal a
+WHERE a.id = 3
+
+-- Get only the customer rows where the `id` field value is 3
+SELECT
+	c.id,
+	c.name,
+	c.address,
+	c.email,
+	c.password
+FROM customer c
+WHERE c.id = 2
+
+SELECT
+	e.id,
+	e.name,
+	e.address,
+	e.location_id
+FROM employee e
+WHERE e.id = 4
+
+SELECT
+	l.id,
+	l.name,
+	l.address
+FROM location l
+WHERE l.id = 1
